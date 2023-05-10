6693a659197bc50b1e8811cf99b4deec2ba7b312
XSnelliusX <markus.goetz17@gmail.com>
2023-05-10 17:38:40 +0200

    use better variable names

--- src/MovieStore/Customer.java
+++ src/MovieStore/Customer.java
@@ -20,12 +20,12 @@ public class Customer {
     }
 
     public String statement() {
-        Enumeration<Rental> enum_rentals = rentals.elements();
+        Enumeration<Rental> enumRentals = rentals.elements();
         StringBuilder result = new StringBuilder("Rental Record for " + this.getName() + "\n");
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
-        while (enum_rentals.hasMoreElements()) {
-            Rental each = enum_rentals.nextElement();
+        while (enumRentals.hasMoreElements()) {
+            Rental each = enumRentals.nextElement();
             //show figures for this rental
             result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(each.getCharge()).append("\n");
         }
