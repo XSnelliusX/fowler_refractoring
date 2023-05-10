ab78515308ffb8a6064e11e54160dc4dc6afd4a9
XSnelliusX <markus.goetz17@gmail.com>
2023-05-10 16:46:12 +0200

    move and rename amountFor

--- src/MovieStore/Customer.java
+++ src/MovieStore/Customer.java
@@ -46,25 +46,8 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
-        double result = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (each.getDaysRented() > 2)
-                    result += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (each.getDaysRented() > 3)
-                    result += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    private double amountFor(Rental aRental) {
+        return aRental.getCharge();
     }
-
 }
     
